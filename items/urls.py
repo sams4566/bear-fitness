08dd966997f6c5e7b49e94df116ca89baead4146
sams4566 <sams4566@gmail.com>
2021-12-20 11:37:35 +0000

    Add item_info.html

--- items/urls.py
+++ items/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_items, name='all_items')
+    path('', views.all_items, name='all_items'),
+    path('<int:item_id>/', views.item_info, name='item_info'),
 ]
--- items/views.py
+++ items/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 
@@ -9,3 +9,12 @@ def all_items(request):
         'items': items,
     }
     return render(request, 'all_items.html', context)
+
+
+def item_info(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+    return render(request, 'item_info.html', context)
