fef4b37e2630c9a48350844866e5da0b04a70b65
sams4566 <sams4566@gmail.com>
2021-12-20 09:35:30 +0000

    Create all_products view

--- items/models.py
+++ items/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    name = models.CharField(max_length=250)
+    bio = models.TextField()
+    sku = models.CharField(max_length=250, blank=True, null=True)
+    cost = models.DecimalField(decimal_places=2, max_digits=6)
+    reviews = models.DecimalField(decimal_places=1, max_digits=3)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- items/views.py
+++ items/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def all_items(request):
+    items = Item.objects.all()
+    
+    context = {
+        'items': items
+    }
+    return render(request, 'all_items.html', context)
