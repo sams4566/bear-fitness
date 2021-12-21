cf66f0b0f58ab3f5ee39b3eccb89567ee86178ec
sams4566 <sams4566@gmail.com>
2021-12-21 16:42:05 +0000

    Create checkout.html and checkout form

---
+++ checkout/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('customer_name', 'email', 'telephone', 'address_line1', 'address_line2', 
+                 'city', 'county', 'country', 'postcode',)
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import OrderForm
 
-# Create your views here.
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket: 
+        messages.error(request, "Add items to the basket to proceed")
+        return render(request, 'all_items.html')
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, template, context)
