1f154c76ab85af03c6385978abcdd7ebc5eb26cb
sams4566 <sams4566@gmail.com>
2021-12-21 11:35:07 +0000

    Add update_basket function and html

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.current_basket, name='current_basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('update/<item_id>/', views.update_basket, name='update_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 
 
@@ -24,3 +24,20 @@ def add_to_basket(request, item_id):
     print(request.session['basket'])
 
     return render(request, 'all_items.html')
+
+
+def update_basket(request, item_id):
+    quantity = 1
+    basket = request.session.get('basket', {})
+    current_size = request.POST['size_id']
+    size = request.POST.get('item_size')
+    
+    if size == current_size:
+        messages.error(request, "That size is already in your basket")
+    else:
+        basket[item_id]['chosen_sizes'][size] = quantity
+        del basket[item_id]['chosen_sizes'][current_size]
+
+    request.session['basket'] = basket
+
+    return redirect(reverse('current_basket'))
