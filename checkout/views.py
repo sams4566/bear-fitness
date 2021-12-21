26a1d9973667138666a208d096e269241a2cfa85
sams4566 <sams4566@gmail.com>
2021-12-21 20:05:25 +0000

    Add stripe payment input

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from .forms import OrderForm
+from django.conf import settings
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+
     basket = request.session.get('basket', {})
-    if not basket: 
-        messages.error(request, "Add items to the basket to proceed")
-        return render(request, 'all_items.html')
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
     }
     return render(request, template, context)
