51916372a376e31d14dba19f8982d2f67742db4b
sams4566 <sams4566@gmail.com>
2021-12-30 17:49:39 +0000

    Filter items by category

--- items/views.py
+++ items/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Item
+from .models import Item, Category
 
 
 def all_items(request):
     items = Item.objects.all()
-    
+    if 'category' in request.GET: 
+            category = request.GET['category'].split(',')
+            items = items.filter(category__name__in=category)
+            category = Category.objects.filter(name__in=category)[0]
+            context = {
+                'items': items,
+                'category': category,
+            }
+            return render(request, 'all_items.html', context)
+    else:
+        items = list(Item.objects.all())
+        def sort_item(item):
+            return item.name
+        items.sort(key=sort_item)
+        all_products_category = 'All Products'
+
     context = {
         'items': items,
+        'all_products_category': all_products_category,
     }
     return render(request, 'all_items.html', context)
 
