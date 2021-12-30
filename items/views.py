0805578184d75b63ed6e080cbe2ad334fdfd52a9
sams4566 <sams4566@gmail.com>
2021-12-30 13:10:18 +0000

    Add related products to item_info.html

--- items/views.py
+++ items/views.py
@@ -13,8 +13,11 @@ def all_items(request):
 
 def item_info(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
+    category = item.category
+    similar_items = Item.objects.all().filter(category=category)
 
     context = {
         'item': item,
+        'similar_items': similar_items,
     }
     return render(request, 'item_info.html', context)
