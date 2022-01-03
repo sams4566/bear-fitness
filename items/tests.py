3a30a1732102b95cfd4886b6fdff5069c356debf
sams4566 <sams4566@gmail.com>
2022-01-03 09:53:45 +0000

    Create first automated test

--- items/tests.py
+++ items/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Item, Category
+from .forms import ItemForm
 
-# Create your tests here.
+
+class TestItemModel(TestCase):
+
+    def test_item_string_gives_back_item_name(self):
+        category = Category.objects.create(name='test_category')
+        item = Item.objects.create(
+            name='test_item',
+            cost='50',
+            reviews='4',
+        )
+        self.assertEqual(str(item), 'test_item')
+
+    
