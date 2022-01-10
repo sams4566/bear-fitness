1ebc423c3bb3b2419f9e7f6aa507783190c99efb
sams4566 <sams4566@gmail.com>
2022-01-10 11:50:29 +0000

    Deploy to Heroku

--- iron_fitness/settings.py
+++ iron_fitness/settings.py
@@ -183,14 +183,14 @@ STRIPE_CURRENCY = 'gbp'
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'iron-fitness1'
     AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     STATICFILES_STORAGE = 'custom_storage.StaticStorage'
-    MEDIAFILES_LOCATION = 'media'
     STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     
