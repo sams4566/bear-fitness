ca2f4fa934a8c7f6bb6999b6ad7411b419a16fa5
sams4566 <sams4566@gmail.com>
2022-12-20 17:48:11 +0000

    Add code for changing away from heroku

--- iron_fitness/settings.py
+++ iron_fitness/settings.py
@@ -30,6 +30,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['iron-fitness1.herokuapp.com', 'localhost']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
