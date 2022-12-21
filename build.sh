b4ef2d387e3ded380f06d3f3afb3868672f5ccce
sams4566 <sams4566@gmail.com>
2022-12-21 18:57:01 +0000

    Add loaddata to build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
  pip install -r requirements.txt
  python manage.py collectstatic --noinput
  python manage.py makemigrations && python manage.py migrate
+ python manage.py loaddata categories.json
+ python manage.py loaddata items.json
